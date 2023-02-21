2e02d51827909b9368161a460261d830b8ddcaac
Emrah Atalay <emrahatalay92@gmail.com>
2023-02-21 11:41:25 +0300

    team api test data dönmesi eklendi.

--- src/pages/api/team/index.ts
+++ src/pages/api/team/index.ts
@@ -1,12 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Team = {
+    id: number
     name: string,
     isActive: boolean
 }
 
-function handler(req: NextApiRequest, res: NextApiResponse<Team>) {
-
+function handler(req: NextApiRequest, res: NextApiResponse<Team[]>) {
+    res.status(200).json([
+        {
+            id: 1,
+            name: "4. Etap Okuma Grubu",
+            isActive: true
+        },
+        {
+            id: 2,
+            name: "2. Etap Okuma Grubu",
+            isActive: true
+        }
+    ]);
 }
 
 export default handler;
